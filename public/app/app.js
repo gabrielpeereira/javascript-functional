b6f2b0a16b212c2b1b04e551fcd70811ebbb57f1
Gabriel <gabriel.developer@outlook.com>
2019-11-17 16:29:39 -0300

    adicionando api fetch

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,10 @@
+import { handleStatus } from './utils/promise-helpers.js';
+
 document
 .querySelector('#myButton')
 .onclick = () => 
     fetch('http://localhost:3000/notas')
-    .then(res => res.json())
+    .then(handleStatus)
     .then(notas => console.log(notas))
     .catch(console.log);
 
