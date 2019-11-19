f24b62a920e23c6fc92252d062094798f15fb901
Gabriel <gabriel.developer@outlook.com>
2019-11-18 21:02:51 -0300

    retornando valores com reduce

--- public/app/app.js
+++ public/app/app.js
@@ -5,6 +5,9 @@ document
 .onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => console.log(notas))
+    .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+    .then(itens => itens.filter(item => item.codigo == '2143'))
+    .then(itens =>  itens.reduce((total, item) => total + item.valor, 0))
+    .then(console.log)
     .catch(console.log);
 
