39abe99389431eba35f56460d6451f9bbea1b848
Gabriel <gabriel.developer@outlook.com>
2019-11-20 23:51:08 -0300

    adicionando promisse

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
 
 document
 .querySelector('#myButton')
@@ -6,8 +6,10 @@ document
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
     .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+    .then(log)
     .then(itens => itens.filter(item => item.codigo == '2143'))
     .then(itens =>  itens.reduce((total, item) => total + item.valor, 0))
+    .then(log)
     .then(console.log)
     .catch(console.log);
 
