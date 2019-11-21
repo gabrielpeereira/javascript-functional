de1285ee8a26522a92edbe084670a3bafaa20e66
Gabriel <gabriel.developer@outlook.com>
2019-11-21 00:07:17 -0300

    adicionando flatmap dentro do projeto

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,16 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
+import './utils/arrays-helpers.js';
 
 document
 .querySelector('#myButton')
 .onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-    .then(log)
-    .then(itens => itens.filter(item => item.codigo == '2143'))
-    .then(itens =>  itens.reduce((total, item) => total + item.valor, 0))
-    .then(log)
+    .then(notas => notas
+        .$flatMap(nota => nota.itens)
+        .filter(item => item.codigo == '2143')
+        .reduce((total, item) => total + item.valor, 0)
+    )
     .then(console.log)
     .catch(console.log);
 
