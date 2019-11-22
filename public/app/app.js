77338a662bf4d52f25e0bfdea941dd3cec58cce3
Gabriel <gabriel.developer@outlook.com>
2019-11-22 00:24:57 -0300

    adicionando intenção dentro de função

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,17 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/arrays-helpers.js';
 
+const sumItems = code => notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
 document
 .querySelector('#myButton')
 .onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => notas
-        .$flatMap(nota => nota.itens)
-        .filter(item => item.codigo == '2143')
-        .reduce((total, item) => total + item.valor, 0)
-    )
+    .then(sumItems('2113'))
     .then(console.log)
     .catch(console.log);
 
